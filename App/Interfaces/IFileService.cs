1237085f6af785f1776e5b0bc748ec9829d27707
Boris Korobeinikov <slon2k@gmail.com>
2020-10-31 22:48:42 +0600

    added: FileService

---
+++ App/Interfaces/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Interfaces
+{
+    public interface IFileService
+    {
+        public string SaveFile(IFormFile file, string folder);
+    }
+}
---
+++ App/Services/FileService.cs
@@ -0,0 +1,34 @@
+﻿using App.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace App.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly IWebHostEnvironment env;
+
+        public FileService(IWebHostEnvironment env)
+        {
+            this.env = env ?? throw new ArgumentNullException(nameof(env));
+        }
+
+        public string SaveFile(IFormFile file, string folder = "uploads")
+        {
+            var uploadPath = Path.Combine(env.WebRootPath, folder);
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            var filePath = Path.Combine(uploadPath, fileName);
+
+            using (var stream = File.Create(filePath))
+            {
+                file.CopyTo(stream);
+            }
+            return fileName;
+        }
+    }
+}
